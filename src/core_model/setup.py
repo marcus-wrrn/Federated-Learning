5d5d24debe1705c0a27e5115b11c0e20cedf0bac
marcus-wrrn <marcuswrrn@gmail.com>
2024-12-02 08:47:23 -0500

    updated core logic structure to fit better as a python package

---
+++ src/core_model/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flcore",
+    version="0.0.1",
+    description="Core Model functionality for the project",
+    author="Marcus Warren",
+    author_email="marcuswrrn@gmail.com",
+    packages=find_packages(),  # Automatically finds all packages in the src directory
+    install_requires=[
+        "torch>=2.5.0",
+        "torchvision>=0.20.0",
+        "matplotlib>=3.9",
+        "pandas"
+    ],
+)
