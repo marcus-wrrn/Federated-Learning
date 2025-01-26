dd96563c617e9eb11c21f64058e37583a580e8d9
Joshua Chen <fac153mc@gmail.com>
2025-01-25 22:22:52 -0500

    Getting accuracy working

--- src/coordination/server/validation.py
+++ src/coordination/server/validation.py
@@ -6,22 +6,22 @@ from torch.utils.data import DataLoader
 from flcore.models.basic import HARSModel
 from flcore.data_handling.datasets import HARSDataset
 import os
-
+import numpy as np
 import torch.utils.data.dataloader
-from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import confusion_matrix
 
-def validation(device,data_path='',model=''):
+def validation(device,data_path='',model='',batchsize=5):
     print("Running validation ...")
-    device = "cpu"
+    device = device
     if torch.cuda.is_available():
         device = "cuda"
     if(data_path == ''):
         data_path = '../data/test.csv'
-    if(type(model)=='str'):
+    if(type(model)== str):
         print("Input is a string")
         if(model == ''):
             print("Error a model needs to be inputted")
-        elif(os.isfile(model)):
+        elif(os.path.isfile(model)):
             print("Load model from file")
             model = load_HARSModel(device,model)
     #else(model.instance())
@@ -30,21 +30,27 @@ def validation(device,data_path='',model=''):
     model.to(device)
     model.eval()
     val_set = HARSDataset(data_path)
-    val_loader = torch.utils.data.dataloader(val_set,batch_size = batchsize)
+    val_loader = torch.utils.data.DataLoader(val_set,batch_size = 1)
 
-    total_corret = 0
+    total_correct = 0
     total_test =0
     for data in val_loader:
         temp = data[0]
         label = data[1]
+        #label = data[1].argmax().item()
         temp.to(device)
-        label.to(device)
+        #label.to(device)
         output = model(temp)
-        pred_label = output.argmax().item()
-        total_correct = total_correct+ (pred_label == label).sum().item()
-        total_test =total_test+  len(pred_label)
+        pred_label = output.argmax(1).tolist()
+        temp2 = output
+        pred_np = np.array(pred_label)
+        label_np = np.array(label.argmax(1).tolist())
+        total_correct = total_correct+ (pred_np == label_np).sum().item()
+        total_test =total_test+  len(label)
 
     model_accuracy = total_correct/total_test 
+    #print(total_test)
+    #print(len(val_set))
     print(model_accuracy)
         
 
@@ -68,12 +74,15 @@ def get_model_true_false(pred,truth,class_list):
     return tp,fp,fn,tn
             
 def load_HARSModel(device,model_path):
-    temp_model = HARSModel
-    if (device == "cpu"):
-        temp_model.load_state_dict(
-            torch.load(model, map_location=torch.device('cpu'), weights_only=True))
-    else:
-        temp_model.load_state_dict(torch.load(model_path, weights_only=True))
+    #print("Loading here")
+    temp_model = HARSModel(device)
+ #   if (device == "cpu"):
+#        temp_model.load_state_dict(
+#            torch.load(temp_model, map_location=torch.device('cpu'), weights_only=True))
+#    else:
+#        temp_model.load_state_dict(torch.load(model_path, weights_only=True))
+    temp_model.load_state_dict(torch.load(model_path, weights_only=True))
+
     temp_model.eval()
     return temp_model
 
@@ -82,7 +91,10 @@ def main():
     device = "cpu"
     temp = HARSModel(device)
     print(type(temp))
-    print(temp.isinstance())
-
+    #print(temp.isinstance())
+    temp2 = "C:/Users/spark/Documents/git/Federated-Learning/src/core_model/flcore/HARSModel2492512025/model.pth"
+    print(type(temp2))
+    print(type(temp2)== str)
 if __name__ == '__main__':
-    main()
+    #main()
+    validation("cpu","C:/Users/spark/Documents/git/Federated-Learning/data/test.csv","C:/Users/spark/Documents/git/Federated-Learning/src/core_model/flcore/HARSModel2492512025/model.pth")
