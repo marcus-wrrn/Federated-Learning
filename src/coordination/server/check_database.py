9587df22600062370d04ec7b4858b6f9ec50491a
jkc140 <fac153mc@gmail.com>
2025-02-11 03:33:18 -0500

    Fixing some issues that arised from removing comments

--- src/coordination/server/check_database.py
+++ src/coordination/server/check_database.py
@@ -14,68 +14,64 @@ def check_database():
         with CoordinationDB(current_app.config["DATAPATH"]) as db:
             # Check database    
             cur_round = db.get_current_round()
-
-            if(cur_round and not finished_agg):
-                print("Training round: ",cur_round.current_round)
-                client_threshold = db.get_round_threshold()
-                client_count = db.get_client_round_num()
-                if(client_count >= client_threshold):
-                    print("Aggregating")
-                    db.update_aggregate(1)
-                    cur_model_id = db.get_model_id(cur_round.current_round)
-
-                    round_path = db.get_model_path(current_app.instance_path,cur_model_id)
-                    cur_model = HARSModel("cpu")
-                    cur_model.load_state_dict(torch.load(round_path))
-                    client_list_states = []
-                    client_ids = db.get_round_client_list2(cur_model_id)
-                    for c_idx in range (0,len(client_ids)):
-                        
-                        client_path = db.get_client_model(current_app.instance_path,client_ids[c_idx][0],cur_model_id)
-                        print("Client id: ",client_ids[c_idx][0])
-                        print("Loading : ",client_path)
-                        client_model = HARSModel("cpu")
-                        client_model.load_state_dict(torch.load(client_path))
-                        client_state = client_model.state_dict()
-                        client_list_states.append(client_state)
-                        db.flag_client_training(client_ids[c_idx][0],cur_model_id,0)
-
-                    aggregate_states = agg_model(client_list_states,cur_model.state_dict())
-
-                    # save the aggregate model         
-                    cur_model.load_state_dict(aggregate_states)
-                    torch.save(cur_model.state_dict(), round_path)
-
-                    max_round = db.get_max_rounds()
-                    db.update_aggregate(0)
-                    if(cur_round.current_round+1 < max_round[0]):
-                        # implement new round logic
-                        db.update_round()
-                        new_round =  db.get_current_round()
-                        new_model_id = db.create_model(new_round.round_id)
-                        model = HARSModel("cpu")
-                        #new_model_id = db.get_model_id(new_round.round_id)
-                        #path = os.path.join(current_app.instance_path, f"super_round_{new_round.super_round_id}/training_round_{new_round.round_id}/{new_model_id}.pth")
-                        path = db.get_model_path(current_app.instance_path,new_model_id)
-                        torch.save(model.state_dict(), path)
-                    else:
-                        print("Max rounds has been hit")
-                        print("Training done")
-                        db.cursor.execute("DELETE FROM training_config WHERE id = 1")
-                        finished_agg = 1
-
-
-                    #print("Finished aggregating ")
+            if (cur_round is None and not finished_agg): continue
                     
+            print("Training round: ",cur_round.current_round)
+            client_threshold = db.get_round_threshold()
+            client_count = db.get_client_round_num()
+            if(client_count >= client_threshold):
+                print("Aggregating")
+                db.update_aggregate(1)
+                cur_model_id = db.get_model_id(cur_round.current_round)
+
+                round_path = db.get_model_path(current_app.instance_path,cur_model_id)
+                cur_model = HARSModel("cpu")
+                cur_model.load_state_dict(torch.load(round_path))
+                client_list_states = []
+                client_ids = db.get_round_client_list2(cur_model_id)
+                for c_idx in range (0,len(client_ids)):
                     
-                    ## Do validation
-                    # # do something with results                
-                    #print("Finish validating results")
+                    client_path = db.get_client_model(current_app.instance_path,client_ids[c_idx][0],cur_model_id)
+                    print("Client id: ",client_ids[c_idx][0])
+                    print("Loading : ",client_path)
+                    client_model = HARSModel("cpu")
+                    client_model.load_state_dict(torch.load(client_path))
+                    client_state = client_model.state_dict()
+                    client_list_states.append(client_state)
+                    db.flag_client_training(client_ids[c_idx][0],cur_model_id,0)
+
+                aggregate_states = agg_model(client_list_states,cur_model.state_dict())
+
+                # save the aggregate model         
+                cur_model.load_state_dict(aggregate_states)
+                torch.save(cur_model.state_dict(), round_path)
+
+                max_round = db.get_max_rounds()
+                db.update_aggregate(0)
+                if(cur_round.current_round+1 < max_round[0]):
+                    # implement new round logic
+                    db.update_round()
+                    new_round =  db.get_current_round()
+                    new_model_id = db.create_model(new_round.round_id)
+                    model = HARSModel("cpu")
+                    #new_model_id = db.get_model_id(new_round.round_id)
+                    #path = os.path.join(current_app.instance_path, f"super_round_{new_round.super_round_id}/training_round_{new_round.round_id}/{new_model_id}.pth")
+                    path = db.get_model_path(current_app.instance_path,new_model_id)
+                    torch.save(model.state_dict(), path)
                 else:
-                    #print("Can't aggregate")
-            else:
-                #print("Not training")
-        
+                    print("Max rounds has been hit")
+                    print("Training done")
+                    db.cursor.execute("DELETE FROM training_config WHERE id = 1")
+                    finished_agg = 1
+
+
+                #print("Finished aggregating ")
+                
+                
+                ## Do validation
+                # # do something with results                
+                #print("Finish validating results")
+
         time.sleep(30)
                 
 
